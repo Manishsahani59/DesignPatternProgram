d5226455a8d4f69f661235fb91c4b60a18f0c386
Manish Sahani <cmc.st.manish@gmail.com>
2019-12-31 16:51:10 +0530

    Initial Design Pattern Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatternProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
